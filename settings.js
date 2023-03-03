05b2774d21f8da00ce5799cf507cb0f04e23b424
Deff sleebew <110314357+Kanata-chan@users.noreply.github.com>
2023-03-03 18:01:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.APIs = {
    alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
 }
 
-// beli apikey premium pc wa: 0887435047326
+// beli apikey premium pc wa: 0883825144098
 // beli apikey premium pc wa: 0887435047326
 global.APIKeys = {
    'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
@@ -41,24 +41,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Lolibot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ Deff Team ×፝֟͜×" //nama kalian
+global.myweb = "https://wa.me/6283825144098" //bebas asal jan hapus
+global.youtube = "-" //bebas asal jan hapus
+global.myweb2 = "-" //bebas
+global.email = "naknolep7229@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone list : https://www.php.net/manual/en/timezones.php 
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6289687889645'] //premium user
+global.owner = ["6289687889645", "6283825144098"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289687889645" // nomor wa kalian
+global.ownernomerr = "+6283825144098" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Loli Bot' //sticker wm ubah
+global.author = 'Di Buat Oleh Deff Team' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'document' //
@@ -233,4 +233,4 @@ fs.watchFile(file, () => {
    console.log(chalk.redBright(`Update'${__filename}'`))
    delete require.cache[file]
    require(file)
-})
+}) Up
